c549730a33f7e06d82aa8a83d0e6f4de6a86f2fe
c-anastacio <60855503+c-anastacio@users.noreply.github.com>
2021-01-29 16:28:43 -0500

    Fields to add

--- models/Post.js
+++ models/Post.js
@@ -1,27 +1,30 @@
-const {model, Schema} = require('mongoose');
+const { model, Schema } = require("mongoose");
 
 const postSchema = new Schema({
-    content: String,
-    username: String,
-    createdAt: String,
-    comments: [
-        {
-            content: String,
-            username: String,
-            createdAt: String
-        }
-    ],
-    upvote: [
-        {
-            username: String,
-            createdAt: String
-        }
-    ],
-    //Helps us later because Mongo is schemaless, build relations with ORM
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-    }
+  content: String,
+  adress: String,
+  unitType: String,
+  rentAmount: String,
+  username: String,
+  createdAt: String,
+  comments: [
+    {
+      content: String,
+      username: String,
+      createdAt: String,
+    },
+  ],
+  upvote: [
+    {
+      username: String,
+      createdAt: String,
+    },
+  ],
+  //Helps us later because Mongo is schemaless, build relations with ORM
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
 });
 
-module.exports = model('Post', postSchema);
+module.exports = model("Post", postSchema);
